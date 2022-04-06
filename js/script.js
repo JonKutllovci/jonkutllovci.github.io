c881b3f0df8ffa9143c7026dfa52aa08cf863ad7
JonKutllovci <jon.kutllovci@gmail.com>
2022-04-07 01:05:35 +0200

    fix_content

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 
-document.querySelectorAll('a').forEach(links =>{
-
-    links.onmouseenter = () =>{
-        cursor1.classList.add('active');
-        cursor2.classList.add('active');
-    }
-
-    links.onmouseleave = () =>{
-        cursor1.classList.remove('active');
-        cursor2.classList.remove('active');
-    }
-
-});
-
 var options = {
   strings: [" Web Developer.", " Web Designer.", " UI/UX Designer."],
   typeSpeed: 40,
